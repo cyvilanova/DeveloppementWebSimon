5abe346f6b2b7bcefefdbc639ec2ac4160a95dca
cvilanova <cyvila@gmail.com>
2019-08-27 21:30:46 -0400

    Guest, admin & client header + links

---
+++ website/headers/admin-header.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Info++</title>
+    <link rel="stylesheet" href="../global.scss">
+    <link rel="stylesheet" href="style.scss">
+
+</head>
+
+<body>
+
+<div class="header">
+    <img src="../../images/icones/logo.png" alt="logo">
+    <nav class="navigation">
+        <div class="top-links">
+            <a href="">Se déconnecter</a>
+        </div>
+
+        <div class="bottom-links">
+            <a href="" class="red">Service</a>
+            <a href="" class="orange">Facture</a>
+            <label>
+                <input class="search">
+            </label>
+        </div>
+    </nav>
+</div>
+
+</body>
+</html>
---
+++ website/headers/guest-header.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Info++</title>
+    <link rel="stylesheet" href="../global.scss">
+    <link rel="stylesheet" href="style.scss">
+
+</head>
+
+<body>
+
+<div class="header">
+    <img src="../../images/icones/logo.png" alt="logo">
+    <nav class="navigation">
+        <div class="top-links">
+            <a href="../login.php">S'identifier</a>
+        </div>
+    </nav>
+</div>
+
+</body>
+</html>
