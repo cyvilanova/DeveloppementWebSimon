222cfaea47ee671fbf3108ebc5f1cb1459737120
cvilanova <cyvila@gmail.com>
2019-09-02 13:52:40 -0400

    Basic form

---
+++ website/user-form/form.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Info++</title>
+    <link rel="stylesheet" href="../global.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="page">
+
+    <form class="form">
+        <div class="text">
+            Remplissez ce forumlaire pour créer votre profil.
+        </div>
+        <div class="subtext">
+            Tous les champs sont obligatoires.
+        </div>
+        <input placeholder="Nom">
+        <input placeholder="Prénom">
+        <input placeholder="No Civic" type="number">
+        <input placeholder="Rue">
+        <select>
+            <option disabled selected>Ville</option>
+            <option>a</option>
+        </select>
+        <input placeholder="Code postal">
+        <input placeholder="Numéro de téléphone" type="number">
+
+        <div class="text">
+            Votre courriel servira à vous identifier lors de votre prochaine visite.
+        </div>
+        <div class="subtext">
+            Le mot de passe doit avoir au moins 1 chiffre, 1 lettre et 8 caractères minimum.
+        </div>
+        <input placeholder="Courriel" type="email">
+        <input placeholder="Confirmation du courriel" type="email">
+        <input placeholder="Mot de passe" type="password">
+        <input placeholder="Confirmation du mot de passe" type="password">
+        <div class="buttons">
+            <button type="submit">Connexion</button>
+        </div>
+    </form>
+</div>
+</body>
+</html>
