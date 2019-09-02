b7fc80eda9683e3e3ca376d9a8f6e81562d9fb7e
cvilanova <cyvila@gmail.com>
2019-09-02 10:09:35 -0400

    Login form

---
+++ website/login/login.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Info++</title>
+    <link rel="stylesheet" href="../global.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="page">
+
+    <div class="text">
+        Veuillez vous identifier pour avoir la possibilité d'acheter des formations.
+    </div>
+
+    <form class="form">
+
+        <div class="inputs">
+            <input type="email" placeholder="Courriel">
+            <input type="password" placeholder="Mot de passe">
+        </div>
+
+        <a href="">Mot de passe oublié</a>
+
+        <div class="buttons">
+            <button type="submit">Connexion</button>
+            <button>S'inscrire</button>
+        </div>
+
+    </form>
+</div>
+
+</body>
+</html>
