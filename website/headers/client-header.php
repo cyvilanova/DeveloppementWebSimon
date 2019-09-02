248ecf8f8c0b62843188d84b5e4e1f6d9be6d7a1
cvilanova <cyvila@gmail.com>
2019-09-02 13:47:51 -0400

    Create different headers with navigation links (#6)
    * Guest, admin & client header + links
    
    * Added search input
    
    * Added line break at end of file

---
+++ website/headers/client-header.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Info++</title>
+    <link rel="stylesheet" href="../global.scss">
+    <link rel="stylesheet" href="style.scss">
+
+</head>
+
+<body>
+
+<div class="header">
+    <img src="../../images/icones/logo.png" alt="logo" class="logo">
+    <nav class="navigation">
+        <div class="top-links">
+            <a href="">Mon panier (1)</a>
+            <a href="">Se déconnecter</a>
+        </div>
+
+        <div class="bottom-links">
+            <a href="" class="red">Catalogue</a>
+            <a href="" class="orange">Profil</a>
+            <label>
+                <input class="search">
+            </label>
+            <img src="../../images/icones/loupe.png" alt="magnifying glass" class="magnifying">
+        </div>
+    </nav>
+</div>
+
+</body>
+</html>
