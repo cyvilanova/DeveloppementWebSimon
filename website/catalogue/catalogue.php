53dd3869284312f29c82d146b1f527ffea2402b6
Tubbs <cyvila@gmail.com>
2019-09-03 12:53:06 -0400

    Card

---
+++ website/catalogue/catalogue.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Info++</title>
+    <link rel="stylesheet" href="../global.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="page">
+
+        <div class="card">
+            <img src="../../images/services/coursexcel.png" alt="Image cours">
+            <div class="informations">
+                <div class="title">
+                    Excel débutant
+                </div>
+                <div class="description">
+                    Ce cours a pour objectif de faire des papates et tester des shits qui serent a rien parce que coliss faut que ca fasse trois lignes. merci.
+                </div>
+                <div class="footer">
+                    <div class="price">
+                        Tarif: 200$
+                    </div>
+                    <div class="duration">
+                        Durée: 25h
+                    </div>
+                    <img src="../../images/icones/panier.png" alt="cart logo" >
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</body>
+
+</html>
