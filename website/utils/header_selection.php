c3930e610167b47e8fc92ccb9708998d3d4e5417
Philippe Pitz Clairoux <philippepitz123@gmail.com>
2019-09-03 12:21:13 -0400

    automatic header fetcher (cont.)

--- website/utils/header_selection.php
+++ website/utils/header_selection.php
@@ -4,6 +4,8 @@ function get_header() {
     if (empty($_SESSION)) {
         include_once $_SERVER['DOCUMENT_ROOT'] . "/website/headers/guest-headers.php";
     } else if ($_SESSION["user"]->getAdministrateur()) {
-        include_once $_SERVER['DOCUMENT_ROOT'] . "/website/headers/guest-headers.php";
+        include_once $_SERVER['DOCUMENT_ROOT'] . "/website/headers/admin-headers.php";
+    } else {
+        include_once $_SERVER['DOCUMENT_ROOT'] . "/website/headers/client-headers.php";
     }
 }
